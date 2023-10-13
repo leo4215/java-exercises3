070e3b858d0843467e93250b5adde62f45e06011
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-13 13:46:37 -0400

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,26 @@
+/* Read an indeterminate amount of X and Y integer values. For each X and Y, display a message that indicates whether these values were typed in ascending or descending order. The reading should be terminated when equal values are given for X and Y. */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert an integer value for X and Y: ");
+        int x = sc.nextInt(), y = sc.nextInt();
+
+        while (x != y) {
+            if (x > y) {
+                System.out.println("Descending");
+            } else {
+                System.out.println("Ascending");
+            }
+
+            System.out.println("Insert a new integer value for X and Y: ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
