7d68f4d73ddfe8c42a583ec7fb3b47ac6b400d0c
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-13 12:12:44 -0400

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,20 @@
+/*Make a program that reads integers until a zero is read. At the end, show the sum of the numbers read.*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = 1, sum = 0;
+
+        while (num != 0) {
+            System.out.println("Insert any integer:");
+            num = sc.nextInt();
+            sum += num;
+        }
+
+        System.out.printf("The sum is: %d", sum);
+        sc.close();
+    }
+}
