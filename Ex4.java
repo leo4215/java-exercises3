c34d8f958afcf2c43f5706447b7aae42f654c2e0
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-13 14:44:43 -0400

    Create Ex4.java

---
+++ Ex4.java
@@ -0,0 +1,22 @@
+/* Write a program that repeats reading a password until it is valid. For each incorrect password, write the message "Invalid Password". When the password is entered correctly, the message "Access allowed" should be printed and the algorithm terminated. Consider that the correct password is the value 2002. */
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert password:");
+        int password = sc.nextInt();
+
+        while (password != 2002) {
+            System.out.println("Invalid password.");
+            System.out.println("");
+            System.out.println("Insert password:");
+            password = sc.nextInt();
+        }
+
+        System.out.println("Access allowed.");
+        sc.close();
+    }
+}
