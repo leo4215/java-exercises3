7ad9454b67128be3a46e530e37704c5ac09c64cb
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-17 10:51:01 -0400

    Create Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,4 +1,4 @@
-/* Make a program to read an indeterminate number of data, each containing an individual's age. The last piece of data, which will not be included in the calculations, contains a negative age value. Calculate and print the average age of this group of individuals. If a negative value is inserted the first time, display the message "Unable to calculate." */
+/* Make a program to read an indeterminate number of data, each containing an individual's age. The last piece of data, which will not be included in the calculations, contains a negative age value. Calculate and print the average age of this group of individuals. If a negative value is entered the first time, display the message "Unable to calculate." */
 
 import java.util.Scanner;
 
@@ -6,7 +6,7 @@ public class Ex3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insert the age:");
+        System.out.println("Enter the age:");
         int age = sc.nextInt(), sum = 0, count = 0;
 
         if (age < 0) {
@@ -15,7 +15,7 @@ public class Ex3 {
             while (age >= 0) {
                 sum += age;
                 count++;
-                System.out.println("Insert the age:");
+                System.out.println("Enter the age:");
                 age = sc.nextInt();
             }
             double total = (double) sum / count;
